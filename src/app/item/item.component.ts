f09eb89fbfcbae0d74b80ef9a75e366fa3ba474c
Sandra Zaniewska <sandra.zaniewska@gmail.com>
2017-09-24 16:37:12 +0200

    todolist

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -6,27 +6,22 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
     <p class='todo-item'>
       <span class='todo-item__title' [class.done]='todoItem.done'>{{ todoItem.title }}</span>
       <input class="todo-checkbox"
-             #checkDone [(ngModel)]="todoItem.done"
-             type='checkbox'
+             #checkDone
+             [(ngModel)]="todoItem.done"
+             type="checkbox"
              data-toggle='toggle'
              (click)="updateItem($event)"/>
-      <button (click)="removeItem($event)">
-        remove
-      </button>
+      <button type="button" (click)="removeItem()">remove</button>
     </p>
   `,
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit {
+export class ItemComponent {
 
   @Input() todoItem: any;
   @Output() remove: EventEmitter<any> = new EventEmitter();
   @Output() update: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
-
-  ngOnInit() {
-  }
   removeItem() {
     this.remove.emit(this.todoItem);
   }
